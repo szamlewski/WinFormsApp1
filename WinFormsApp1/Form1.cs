3d5aaaff2fe366fe91cb1783ea151825eeb2b18f
Bartosz Szamlewski <bartosz.szamlewski@teb.edu.pl>
2023-09-06 12:35:17 +0200

    Poruszanie się guzika po ekranie

--- WinFormsApp1/Form1.cs
+++ WinFormsApp1/Form1.cs
@@ -9,7 +9,31 @@ namespace WinFormsApp1
 
         private void CenterButton_Click(object sender, EventArgs e)
         {
-            Application.Exit();
+            //Application.Exit();
+
+            //szerokosc okna aplikacji
+            int windowWidth = Size.Width;
+            //wysokosc okna aplikacji
+            int windowHeight = Size.Height;
+
+            //zeby uniknac sytuacji, kiedy guzik wyladuje poza oknem aplikacji, odejmujemy jego szerokosc i wysokosc odpowiednio
+            windowWidth -= CenterButton.Width;
+            windowHeight -= CenterButton.Height;
+
+            //inicjalizacja generatora losowych liczb
+            Random rnd = new Random();
+
+            //losowa odleglosc od lewej
+            int randomLeft = rnd.Next(windowWidth);
+
+            //analogicznie z odlegloscia od gory
+            int randomTop = rnd.Next(windowHeight);
+
+            //przesuwamy guzik
+            //location musi byc przekazany jako punkt na ekranie
+            Point topLeftCorner = new Point(randomLeft, randomTop);
+            //CenterButton no nazwa (atrybut Name) mojego guzika
+            CenterButton.Location = topLeftCorner;
         }
     }
 }
