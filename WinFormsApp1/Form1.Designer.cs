1770fe89b82bbe4ce4e7f95c22c48459ca81f170
Bartosz Szamlewski <bartosz.szamlewski@teb.edu.pl>
2023-09-13 12:05:35 +0200

    timer do guzika

--- WinFormsApp1/Form1.Designer.cs
+++ WinFormsApp1/Form1.Designer.cs
@@ -28,37 +28,69 @@
         /// </summary>
         private void InitializeComponent()
         {
+            components = new System.ComponentModel.Container();
             CenterButton = new Button();
+            timer1 = new System.Windows.Forms.Timer(components);
+            statusStrip1 = new StatusStrip();
+            toolStripStatusLabel1 = new ToolStripStatusLabel();
+            statusStrip1.SuspendLayout();
             SuspendLayout();
             // 
             // CenterButton
             // 
             CenterButton.BackColor = Color.OrangeRed;
             CenterButton.Font = new Font("Segoe UI", 20F, FontStyle.Bold, GraphicsUnit.Point);
-            CenterButton.Location = new Point(460, 206);
+            CenterButton.Location = new Point(898, 425);
             CenterButton.Margin = new Padding(3, 2, 3, 2);
             CenterButton.Name = "CenterButton";
-            CenterButton.Size = new Size(262, 206);
+            CenterButton.Size = new Size(181, 58);
             CenterButton.TabIndex = 0;
             CenterButton.Text = "złap mnie";
             CenterButton.UseVisualStyleBackColor = false;
             CenterButton.Click += CenterButton_Click;
+            // 
+            // timer1
+            // 
+            timer1.Interval = 3000;
+            timer1.Tick += timer1_Tick_1;
+            // 
+            // statusStrip1
+            // 
+            statusStrip1.Items.AddRange(new ToolStripItem[] { toolStripStatusLabel1 });
+            statusStrip1.Location = new Point(0, 596);
+            statusStrip1.Name = "statusStrip1";
+            statusStrip1.Size = new Size(1159, 22);
+            statusStrip1.TabIndex = 2;
+            statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            toolStripStatusLabel1.Size = new Size(56, 17);
+            toolStripStatusLabel1.Text = "Punkty: 0";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 14F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1159, 618);
+            Controls.Add(statusStrip1);
             Controls.Add(CenterButton);
             Font = new Font("Algerian", 9F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
             Margin = new Padding(3, 2, 3, 2);
             Name = "Form1";
             Text = "Form1";
+            statusStrip1.ResumeLayout(false);
+            statusStrip1.PerformLayout();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private Button CenterButton;
+        private System.Windows.Forms.Timer timer1;
+        private StatusStrip statusStrip1;
+        private ToolStripStatusLabel toolStripStatusLabel1;
     }
 }
--- WinFormsApp1/Form1.cs
+++ WinFormsApp1/Form1.cs
@@ -2,38 +2,69 @@ namespace WinFormsApp1
 {
     public partial class Form1 : Form
     {
+        Random r;
+        int punkty;
         public Form1()
         {
+
             InitializeComponent();
+            r = new Random();
+            punkty = 0;
+            timer1.Start();
         }
+
 
         private void CenterButton_Click(object sender, EventArgs e)
         {
+            MoveButton();
+            punkty++;
+            toolStripStatusLabel1.Text = "Punkty: " + punkty.ToString();
+            timer1.Stop();
+            timer1.Start();
+           
+
             //Application.Exit();
+            
+            /* //szerokosc okna aplikacji
+             int windowWidth = Size.Width;
+             //wysokosc okna aplikacji
+             int windowHeight = Size.Height;
 
-            //szerokosc okna aplikacji
-            int windowWidth = Size.Width;
-            //wysokosc okna aplikacji
-            int windowHeight = Size.Height;
+             //zeby uniknac sytuacji, kiedy guzik wyladuje poza oknem aplikacji, odejmujemy jego szerokosc i wysokosc odpowiednio
+             windowWidth -= CenterButton.Width;
+             windowHeight -= CenterButton.Height;
 
-            //zeby uniknac sytuacji, kiedy guzik wyladuje poza oknem aplikacji, odejmujemy jego szerokosc i wysokosc odpowiednio
-            windowWidth -= CenterButton.Width;
-            windowHeight -= CenterButton.Height;
+             //inicjalizacja generatora losowych liczb
+             Random rnd = new Random();
 
-            //inicjalizacja generatora losowych liczb
-            Random rnd = new Random();
+             //losowa odleglosc od lewej
+             int randomLeft = rnd.Next(windowWidth);
 
-            //losowa odleglosc od lewej
-            int randomLeft = rnd.Next(windowWidth);
+             //analogicznie z odlegloscia od gory
+             int randomTop = rnd.Next(windowHeight);
 
-            //analogicznie z odlegloscia od gory
-            int randomTop = rnd.Next(windowHeight);
+             //przesuwamy guzik
+             //location musi byc przekazany jako punkt na ekranie
+             Point topLeftCorner = new Point(randomLeft, randomTop);
+             //CenterButton no nazwa (atrybut Name) mojego guzika
+             CenterButton.Location = topLeftCorner; */
+        }
 
-            //przesuwamy guzik
-            //location musi byc przekazany jako punkt na ekranie
-            Point topLeftCorner = new Point(randomLeft, randomTop);
-            //CenterButton no nazwa (atrybut Name) mojego guzika
-            CenterButton.Location = topLeftCorner;
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            MoveButton();
+            punkty = 0;
+            toolStripStatusLabel1.Text = "Punkty: " + punkty.ToString();
+        }
+
+        private void MoveButton()
+        {
+            int maxX = this.Size.Width - CenterButton.Size.Width - 50;
+            int maxY = this.Size.Height - CenterButton.Size.Height - 50;
+            Point p = new Point();
+            p.X = r.Next(12, maxX);
+            p.Y = r.Next(12, maxY);
+            CenterButton.Location = p;
         }
     }
 }
